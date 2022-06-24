31a838c3432c18c42eb325ce3b816b0d2708c84c
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-06-24 13:07:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ global.sgh = process.env.LINK_GITHUB
 global.sgc = process.env.LINK_GROUP
 global.ownernumber = process.env.NOWNER
 global.owner = [
-  ['6282287750102'],
-  ['6281268184624'],
-  [`${6282287750102}`, `${process.env.NAMAOWNER}`, true]
+  ['0'],
+  ['0'],
+  [`${process.env.NOWNER}`, `${process.env.NAMAOWNER}`, true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [`${process.env.MODERATOR}`] // Want some help?
